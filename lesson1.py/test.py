34d16d7fff5fc4e4d8691ac25a968d09cfb4a3b8
funcydev <ramzeskin@mail.ru>
2025-09-10 00:06:25 +0500

    v2

--- lesson1.py/test.py
+++ lesson1.py/test.py
@@ -16,6 +16,28 @@
 # print(first_name, last_name, str(age))
 # print(f'{first_name} {last_name} {age}')
 
-name = input('Type your name dude!: ')
-print('Hello', name, 'How are you?')
-print(f'{name}, are you okay')
+# name = input('Type your name dude!: ')
+# print('Hello', name, 'How are you?')
+# print(f'{name}, are you okay')
+
+# num1 = int(input("Type first number: "))
+# num2 = int(input("Type second number: "))
+# #print(num1 + num2)
+# print(f'{num1}+{num2}={num1+num2}')
+
+result1 = 5 + 3
+result2 = 5 - 3
+result3 = 5 * 3
+result4 = 5 / 3
+result8 = 5 % 3    #ostatok ot deleniya
+result5 = 5 ** 3
+result6 = 5 ** 0.5
+result7 = 10 // 3
+print(type(result4))
+print(int(result4))
+
+print(f' result1 = {result1}\n result2 = {result2}\n result3 = {result3} \n result4 = {result4} \n result5 = {result5} \n result6 = {result6} \n result7 = {result7} \n result8 = {result8} ')
+
+a = 1
+a = a + 1
+print(a)
