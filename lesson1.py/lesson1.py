f7cab487c4154d92bff59baf817eb64fce39eb32
Ramil <ramil.yunusov@xb.mail.uz>
2025-09-09 17:50:02 +0500

    v1

---
+++ lesson1.py/lesson1.py
@@ -0,0 +1,16 @@
+'''
+Напишите программу, которая спрашивает имя пользователя и затем приветствует его
+'''
+
+# print("Enter your name: ")
+# name = input()
+# print('Hello,', name)
+
+'''
+Напишите программу, которая считывает три числа и выводит их сумму.
+'''
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print(a + b + c)
+
---
+++ lesson1.py/test.py
@@ -0,0 +1,21 @@
+# name = input("What is your name?: ")
+# print(f"Hello {name}")
+#
+# a = 2
+# b = 4
+# print(a+b)
+
+# print('Hello great programmer!')
+
+# first_name = 'Ramil'
+# last_name = 'Yunusov'
+# age = 19
+# print(type(age))
+# print(first_name + ' ' + last_name + ' ' + str(age))
+# #or
+# print(first_name, last_name, str(age))
+# print(f'{first_name} {last_name} {age}')
+
+name = input('Type your name dude!: ')
+print('Hello', name, 'How are you?')
+print(f'{name}, are you okay')
