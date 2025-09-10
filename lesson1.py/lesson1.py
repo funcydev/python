ba2e9af7f25d026d0a60333d4cdf3d4aedc77661
Ramil <ramil.yunusov@xb.mail.uz>
2025-09-10 10:55:29 +0500

    v2

--- lesson1.py/lesson1.py
+++ lesson1.py/lesson1.py
@@ -1,16 +1,27 @@
 '''
-Напишите программу, которая спрашивает имя пользователя и затем приветствует его
+1) Напишите программу, которая спрашивает имя пользователя и затем приветствует его
 '''
 
-# print("Enter your name: ")
-# name = input()
+# name = input("Enter your name: ")
 # print('Hello,', name)
 
 '''
-Напишите программу, которая считывает три числа и выводит их сумму.
+2) Напишите программу, которая считывает три числа и выводит их сумму.
 '''
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-print(a + b + c)
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+# print(f'Summ of numbers {a}, {b} and {c} is: {a + b + c}')
 
+'''
+3) Напишите программу, для решения следующей задачи: n школьников делят k яблок поровну,
+неделящийся остаток остается в корзинке. Сколько яблок достанется каждому школьнику? 
+Сколько яблок останется в корзинке? Программа получает на вход числа n и k и должна вывести 
+искомое количество яблок (два числа).
+'''
+pupil_num = int(input("Enter amount of pupile: "))
+apple_num = int(input("Enter amount of apples: "))
+each_get = apple_num // pupil_num
+# remain = apple_num
+print (f'Each pupil got {each_get} apple')
+print (f'')
