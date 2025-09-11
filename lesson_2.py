c2b72de49bd14ab5515e5aaab1fc6d348e4f1ddf
Ramil <ramil.yunusov@xb.mail.uz>
2025-09-11 18:09:03 +0500

    v1

---
+++ lesson_2.py
@@ -0,0 +1,62 @@
+'''
+1) Составить три формы присвоение следующего вида x, y, z = y, z, x (придумать способ применения )
+
+
+'''
+
+
+
+
+'''
+2) Распечатать: сложение, вычитание, умножение, деление, возведение в степень следующих переменных:
+
+num1 = '3.14'
+num2 = '4'
+'''
+# num1 = '3.14'
+# num2 = '4'
+# num3, num4 = (float(num1)), (int(num2))
+# num5 = str(num3 + num4)
+# num6 = str(num3 - num4 - 0.001)
+# num7 = str(num3 * num4)
+# num8 = str(num3 / num4)
+# num9 = str(num3 ** num4)
+# print(f'{num1} + {num2} = {num5[0:4]}')
+# print(f'{num1} - {num2} = {num6[0:5]}')
+# print(f'{num1} * {num2} = {num7[0:6]}')
+# print(f'{num1} / {num2} = {num8[0:5]}')
+# print(f'{num1} ** {num2} = {num9[0:5]}')
+
+'''
+3) Воспользуйтесь различными методами строк
+str1 = ' << pYthOn -   '
+str2 = '   pYthOn \n .   '
+str3 = ' (( pYthOn - :+   '
+'''
+# str1 = ' << pYthOn -   '
+# str2 = '   pYthOn \n .   '
+# str3 = ' (( pYthOn - :+   '
+# print(str1[4:10].lower())
+# print(str2[3:9].upper())
+# print(str3[4:10].capitalize())
+
+'''
+4) Обработайте каждую переменную и получите результат как на картинке:
+'''
+string1 = 'I love python'
+string2 = 'Hello my dear friend'
+string3 = 'полиморфизм'
+print(string1[-1:])
+'''
+5)
+'''
+
+
+'''
+6)
+'''
+
+
+'''
+7)
+'''
