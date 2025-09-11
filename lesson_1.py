99431198087d41193f4e2426eb51a2173c76d1a9
Ramil <ramil.yunusov@xb.mail.uz>
2025-09-11 11:44:39 +0500

    v6

--- lesson_1.py
+++ lesson_1.py
@@ -2,16 +2,16 @@
 1) Напишите программу, которая спрашивает имя пользователя и затем приветствует его
 '''
 
-# name = input("Enter your name: ")
-# print('Hello,', name)
+name = input("Enter your name: ")
+print('Hello,', name)
 
 '''
 2) Напишите программу, которая считывает три числа и выводит их сумму.
 '''
-# a = int(input("Enter first number: "))
-# b = int(input("Enter second number: "))
-# c = int(input("Enter third number: "))
-# print(f'Summ of numbers {a}, {b} and {c} is: {a + b + c}')
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print(f'Summ of numbers {a}, {b} and {c} is: {a + b + c}')
 
 '''
 3) Напишите программу, для решения следующей задачи: n школьников делят k яблок поровну,
@@ -19,12 +19,12 @@
 Сколько яблок останется в корзинке? Программа получает на вход числа n и k и должна вывести 
 искомое количество яблок (два числа).
 '''
-# pupil_num = int(input("Enter amount of pupile: "))
-# apple_num = int(input("Enter amount of apples: "))
-# each_get = apple_num // pupil_num
-# remain = apple_num % pupil_num
-# print (f'Each pupil got {each_get} apple')
-# print (f'Remains in basket: {remain} apple')
+pupil_num = int(input("Enter amount of pupile: "))
+apple_num = int(input("Enter amount of apples: "))
+each_get = apple_num // pupil_num
+remain = apple_num % pupil_num
+print (f'Each pupil got {each_get} apple')
+print (f'Remains in basket: {remain} apple')
 
 '''
 4) Напишите программу, которая спрашивает число у пользователя и выводит следующее:
@@ -32,22 +32,22 @@
 Следующее число для 126 --> 127
 Предыдущее число для 126 --> 125
 '''
-# enter_num = int(input("Enter number: "))
-# next_num = enter_num + 1
-# prev_num = enter_num - 1
-# print(f'Next number for {enter_num} --> {next_num}')
-# print(f'Previous number for {enter_num} --> {prev_num}')
+enter_num = int(input("Enter number: "))
+next_num = enter_num + 1
+prev_num = enter_num - 1
+print(f'Next number for {enter_num} --> {next_num}')
+print(f'Previous number for {enter_num} --> {prev_num}')
 
 '''
 5) Напишите программу, которая выводит текст в данном формате
 
 '''
-# print('Twinkle, twinkle, little star,\n\t'
-#       'How I wonder what you are!\n\t\t\t'
-#       'Up above the world so high,\n\t\t\t'
-#       'Like a diamond in the sky.\n'
-# 'Twinkle, twinkle, little star,\n\t\t'
-# 'How I wonder what you are')
+print('Twinkle, twinkle, little star,\n\t'
+      'How I wonder what you are!\n\t\t\t'
+      'Up above the world so high,\n\t\t\t'
+      'Like a diamond in the sky.\n'
+'Twinkle, twinkle, little star,\n\t\t'
+'How I wonder what you are')
 
 '''
 6) Напишите программу для использования метода форматирования следующих трех переменных
@@ -55,15 +55,15 @@
 
 У меня есть 1000 долларов, поэтому я могу купить 3 футбольных мяча за 450 долларов
 '''
-# totalMoney = 1000
-# quantity = 3
-# price = 450
-# print(f'I have got {totalMoney} dollars, this why a can buy {quantity} footballs for {price} dollars each')
+totalMoney = 1000
+quantity = 3
+price = 450
+print(f'I have got {totalMoney} dollars, this why a can buy {quantity} footballs for {price} dollars each')
 
 '''
 7) Создайте две переменных, со значением ‘spam’ и ‘eggs’, затем распечатайте объединив две переменной в одну целую
 '''
-# a = 'spam'
-# b = 'eggs'
-# c = a + b
-# print(c)
+a = 'spam'
+b = 'eggs'
+c = a + b
+print(c)
