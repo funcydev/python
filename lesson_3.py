1e9c911973bfcb2d864bb214f12bbc522ba342be
Ramil <ramil.yunusov@xb.mail.uz>
2025-09-25 10:30:42 +0500

    v3

--- lesson_3.py
+++ lesson_3.py
@@ -12,15 +12,42 @@
 # else:
 #     print(f'Number of coupe: {number // 4}')
 
-city = input('Enter city name: ').lower()
-if not city:
-   cnt = len(city)
-   if city == 'tashkent'[0:cnt]:
-        print('Sunny day in Tashkent')
-   elif city == 'london'[0:cnt]:
-        print('Renny day in London')
-   elif city == 'paris'[0:cnt]:
-        print('Lovely day in Paris')
-   else:
-        print("XZ")
-print('You did not set city name')
+# city = input('Enter city name: ').lower()
+# if not city:
+#    cnt = len(city)
+#    if city == 'tashkent'[0:cnt]:
+#         print('Sunny day in Tashkent')
+#    elif city == 'london'[0:cnt]:
+#         print('Renny day in London')
+#    elif city == 'paris'[0:cnt]:
+#         print('Lovely day in Paris')
+#    else:
+#         print("XZ")
+# print('You did not set city name')
+
+
+
+# from datetime import datetime
+# current_date = datetime.now()
+# # print(current_date)
+# current_date2 = datetime.now().strftime("%d")
+# # print(current_date2)
+# day = int(current_date2)
+# if   day <= 10:
+#     print('Milk')
+# elif day <= 20:
+#     print('Cheese')
+# elif day <= 28:
+#     print('Meat')
+# else:
+#     print('Holiday')
+
+# a = 3
+# num = 0 if a % 4 == 0 else 1
+# print(num)
+
+# a = 15
+# b = 10
+# print('a bigger then b') if a > b else print('b bigger then a')
+
+
